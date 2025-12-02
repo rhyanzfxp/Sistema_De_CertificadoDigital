abd58265e72f783ca9ef0c8a9ec721e8781e6bfa
rhyanzfxp <rhyandarocha2006@gmail.com>
2025-12-02 10:20:51 -0300

    Deploy

--- utils/baseUrl.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function getBaseUrl(req) {
-  const configured = process.env.BASE_URL_EXTERNA;
-  if (configured) return configured.replace(/\/$/, '');
-
-  const proto = (req.headers['x-forwarded-proto'] || req.protocol || 'http').split(',')[0];
-  const host = req.headers['x-forwarded-host'] || req.headers.host;
-  return `${proto}://${host}`.replace(/\/$/, '');
-};
